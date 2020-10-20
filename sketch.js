59c0599864efd830b825c9700d22a33d6849324b
albatross7079 <70518748+albatros7079@users.noreply.github.com>
2020-10-20 17:08:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var ball;
+var database;
+function setup(){
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    database= firebase.database();
+    var ballPosition = database.ref('Ball_Pos');
+    ballPosition.on("value", readPosition);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    ball.x = ball.x + x;
+    ball.y = ball.y + y;
+}
+function readPosition(data) {
+    var position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+
+}
